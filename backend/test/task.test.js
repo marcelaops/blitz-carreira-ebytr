2e95886386a9c2537368fce1ad35d8f347c7c4c5
Marcela Silva <marcela.ops@hotmail.com>
2022-02-16 12:01:42 -0300

    test: install frisby

---
+++ backend/test/task.test.js
@@ -0,0 +1,11 @@
+/* eslint-disable mocha/no-mocha-arrows */
+require('dotenv').config();
+
+// const mongoDbUrl = `mongodb://${process.env.HOST || 'mongodb'}:27017/ToDoList`;
+// const url = 'http://localhost:3000';
+
+describe('Insira uma nova tarefa na lista', () => {
+  beforeAll(async () => {
+
+  });
+});
