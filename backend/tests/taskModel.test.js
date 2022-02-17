7ed9d682d0ae3b55cfe222ac58482a03c71191fc
Marcela Silva <marcela.ops@hotmail.com>
2022-02-17 09:36:11 -0300

    feat: `taskModel.test.js`, `taskModel.js` & i mongodb-memory-server@6

--- backend/tests/taskModel.test.js
+++ backend/tests/taskModel.test.js
@@ -1,24 +1,40 @@
 /* eslint-disable mocha/no-mocha-arrows */
+
+// Para escrever esses testes foram usados como referência o conteúdo do dia 27.4 da Trybe e o projeto Store Manager, também da Trybe.
+
+const sinon = require('sinon');
 const { expect } = require('chai');
+const { MongoClient } = require('mongodb');
+const { getConnection } = require('./mongoMockConnection');
+const TasksModel = require('../models/taskModel');
+// const connection = require('../models/connection');
 
-const TasksModel = {
-  create: () => {},
-};
-
-describe('Insira uma nova tarefa no banco de dados', () => {
+describe('Será validado que é possível criar uma tarefa com sucesso', () => {
+  let connectionMock;
   const mockTask = { name: 'task description' };
 
-  describe('Será validado que é possível criar uma tarefa com sucesso', () => {
-    it('Ao adicionar a tarefa o retorno é um objeto', async () => {
-      const response = await TasksModel.create(mockTask);
-      
-      expect(response).to.be.a('object');
-    });
+  // Mockando a conexão sem subir nada para o MongoDb -> características reais porem sem a conexão real.
+  before(async () => {
+    connectionMock = await getConnection();
 
-    it('O obajeto retornado possui o "_id" da tarefa inserida', async () => {
-      const response = await TasksModel.create(mockTask);
-      
-      expect(response.to.have.a.property('_id'));
-    });
+    sinon.stub(MongoClient, 'connect').resolves(connectionMock);
+  });
+
+  after(async () => {
+    await connectionMock.db('TaskManager').collection('tasks').drop();
+
+    MongoClient.connect.restore();
+  });
+
+  it('Ao adicionar a tarefa o retorno é um objeto', async () => {
+    const response = await TasksModel.create(mockTask);
+    
+    expect(response).to.be.a('object');
+  });
+
+  it('O objeto retornado possui o "_id" da tarefa inserida', async () => {
+    const response = await TasksModel.create(mockTask);
+    
+    expect(response).to.have.a.property('_id');
   });
 });
